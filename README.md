61ee0e045bb2441a888c82847aff6f3bd0014385
fidanguliyeva <f2guliyeva@gmail.com>
2023-07-29 12:52:07 +0400

    created startNewRound functionality and updated README file

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
-# Blackjack game
+# Blackjack game rules
 
---
+The objective of Blackjack is to beat the dealer's hand without exceeding a total card value of 21. Player competes only against the dealer.
 
-## Game rules
+## Card Values 
 
+- Number cards (2 to 10) are worth their face value.
+- Face cards (Jack, Queen, King) are worth 10 points each.
+- Aces can be worth either 1 or 11 points, bit for simplicty in this game they are worth 11. 
 
+## Gameplay
+1. **Initial Deal**: The player and the dealer receive two face-up cards.
+
+2. **Player's Turn**: The player has the following options:
+    - **Hit**: Take an additional card from the deck to increase the total value of their hand.
+    - **Stand**: Keep the current hand and end their turn.
+
+3. **Dealer's Turn**: After a player has completed their turn, the dealer must:
+    - hit on a hand totaling 16 or less
+    - stand on a hand totaling 17 or more
+
+4. **Winning and Losing**:
+    - If the player's total exceeds 21, they "bust" and automatically lose the round.
+    - If the dealer busts and the player does not, the player wins.
+    - If neither the player nor the dealer busts, the hand with the highest total value (up to 21) wins.
+    - If the player's and dealer's hands have the same value, it's a "push," and the player keeps their bet without winning additional money.
+
+5. **Blackjack**: If a player's first two cards are an Ace and a 10-point card (10, Jack, Queen, or King), they have a "Blackjack" or a "Natural." This typically pays out at a higher rate than a regular win.
+
+6. **Insurance**: If the dealer's face-up card is an Ace, players have the option to take insurance against the dealer having a Blackjack. Insurance costs half the original bet and pays 2 to 1 if the dealer does have Blackjack.
+
+7. **Surrender**: In some variations, players may have the option to surrender and forfeit half of their bet if they believe their hand is weak.
+
+## Winning and Payouts
+- Winning regular hands (non-Blackjack) pays 1 to 1, meaning you win an equal amount to your bet.
+- Winning with a Blackjack typically pays 3 to 2, meaning you win 1.5 times your bet.
--- lib/screens/black_jack_screen.dart
+++ lib/screens/black_jack_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "dart:math";
 
 class BlackJackScreen extends StatefulWidget {
   const BlackJackScreen({Key? key}) : super(key: key);
@@ -10,6 +11,154 @@ class BlackJackScreen extends StatefulWidget {
 class _BlackJackScreenState extends State<BlackJackScreen> {
   bool _isGameStarted = false;
 
+  // Card Images
+  List<Image> myCards = [];
+  List<Image> dealersCards = [];
+
+  // Cards
+  String? dealerFirstCard;
+  String? dealerSecondCard;
+
+  String? playerFirstCard;
+  String? playerSecondCard;
+
+
+  //Scores
+  int dealerScore = 0;
+  int playerScore = 0;
+
+  // Deck of cards
+  final Map<String, int> deckOfCards = {
+    "cards/2.1.png": 2,
+    "cards/2.2.png": 2,
+    "cards/2.3.png": 2,
+    "cards/2.4.png": 2,
+    "cards/3.1.png": 3,
+    "cards/3.2.png": 3,
+    "cards/3.3.png": 3,
+    "cards/3.4.png": 3,
+    "cards/4.1.png": 4,
+    "cards/4.2.png": 4,
+    "cards/4.3.png": 4,
+    "cards/4.4.png": 4,
+    "cards/5.1.png": 5,
+    "cards/5.2.png": 5,
+    "cards/5.3.png": 5,
+    "cards/5.4.png": 5,
+    "cards/6.1.png": 6,
+    "cards/6.2.png": 6,
+    "cards/6.3.png": 6,
+    "cards/6.4.png": 6,
+    "cards/7.1.png": 7,
+    "cards/7.2.png": 7,
+    "cards/7.3.png": 7,
+    "cards/7.4.png": 7,
+    "cards/8.1.png": 8,
+    "cards/8.2.png": 8,
+    "cards/8.3.png": 8,
+    "cards/8.4.png": 8,
+    "cards/9.1.png": 9,
+    "cards/9.2.png": 9,
+    "cards/9.3.png": 9,
+    "cards/9.4.png": 9,
+    "cards/10.1.png": 10,
+    "cards/10.2.png": 10,
+    "cards/10.3.png": 10,
+    "cards/10.4.png": 10,
+    "cards/J1.png": 10,
+    "cards/J2.png": 10,
+    "cards/J3.png": 10,
+    "cards/J4.png": 10,
+    "cards/Q1.png": 10,
+    "cards/Q2.png": 10,
+    "cards/Q3.png": 10,
+    "cards/Q4.png": 10,
+    "cards/K1.png": 10,
+    "cards/K2.png": 10,
+    "cards/K3.png": 10,
+    "cards/K4.png": 10,
+    "cards/A1.png": 11,
+    "cards/A2.png": 11,
+    "cards/A3.png": 11,
+    "cards/A4.png": 11,
+  };
+
+  Map<String, int> playingCards = {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    playingCards.addAll(deckOfCards);
+  }
+
+  // Reset the round and reset cards
+  void startNewRound() {
+
+    _isGameStarted = true;
+
+    // start new round with full deck of cards
+    playingCards = {};
+    playingCards.addAll(deckOfCards);
+
+    // reset the player and dealer cards
+    myCards = [];
+    dealersCards = [];
+
+    // Get first random card for dealer
+    Random random = Random();
+    String cardOneKey = playingCards.keys.elementAt(random.nextInt(playingCards.keys.length));
+
+    // remove the selected first card from the deck of playing cards
+    playingCards.removeWhere((key, value) => key == cardOneKey);
+
+    // Get second random card for dealer
+    String cardTwoKey = playingCards.keys.elementAt(random.nextInt(playingCards.keys.length));
+
+    // remove the selected second card from the deck of playing cards
+    playingCards.removeWhere((key, value) => key == cardTwoKey);
+
+    // Get first random card for player
+    String cardThreeKey = playingCards.keys.elementAt(random.nextInt(playingCards.keys.length));
+
+    // remove the third card from the playing cards deck
+    playingCards.removeWhere((key, value) => key == cardThreeKey);
+
+    // Get second random card for the player
+    String cardFourKey = playingCards.keys.elementAt(random.nextInt(playingCards.keys.length));
+
+    // remove the fourth card from the playing cards deck
+    playingCards.removeWhere((key, value) => key == cardFourKey);
+
+    // Assign cards to dealer and player
+    dealerFirstCard = cardOneKey;
+    dealerSecondCard = cardTwoKey;
+    
+    playerFirstCard = cardThreeKey;
+    playerSecondCard = cardFourKey;
+
+    // Adding dealers cards images to display in the grid view
+    dealersCards.add(Image.asset(dealerFirstCard!));
+    dealersCards.add(Image.asset(dealerSecondCard!));
+
+    // Adding dealers cards images to display in the grid view
+    myCards.add(Image.asset(playerFirstCard!));
+    myCards.add(Image.asset(playerSecondCard!));
+
+    // Calculate the score of the dealer and player
+    dealerScore = deckOfCards[dealerFirstCard]! + deckOfCards[dealerSecondCard]!;
+    playerScore = deckOfCards[playerFirstCard]! + deckOfCards[playerSecondCard]!;
+
+    setState(() {});
+  }
+
+  // Add extra card to player (hit)
+  void addCard() {
+
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +169,14 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
 
                   Column(
                     children: [
-                      const Text("Dealer's score"),
+                      Text("Dealer's score: $dealerScore"),
                       Container(
                         height: 300,
                         child: GridView.builder(
                             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                            itemCount: 0,
+                            itemCount: dealersCards.length,
                             itemBuilder: (context, index) {
-                              // TODO: return dealer's cards here
-                              return Container();
+                              return dealersCards[index];
                             }
                         ),
                       )],
@@ -36,15 +184,14 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
 
                   Column(
                     children: [
-                      const Text("Player's score"),
+                      Text("Player's score: $playerScore"),
                       Container(
                         height: 300,
                         child: GridView.builder(
                             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                            itemCount: 0,
+                            itemCount: myCards.length,
                             itemBuilder: (context, index) {
-                              // TODO: return player's cards here
-                              return Container();
+                              return myCards[index];
                             }
                         ),
                       )
@@ -54,13 +201,17 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
                   Column(
                     children: [
                       MaterialButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            addCard();
+                          },
                           color: const Color(0XFFEC625F),
                           child: const Text("Another card"),
 
                       ),
                       MaterialButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            startNewRound();
+                          },
                           color: const Color(0XFFEC625F),
                           child: const Text("Next Round"),
                       )
@@ -79,7 +230,7 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
               height: 60,
               onPressed: () {
                 setState(() {
-                  _isGameStarted = true;
+                  startNewRound();
                 });
               },
               child: const Text("Start Game"),
